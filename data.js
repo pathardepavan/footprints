998ff6ceae510e8173f1460e5fafd8c99da564f9
Ankit Jain <jankit@vmware.com>
2018-01-23 17:25:22 +0530

    Adding data

---
+++ data.js
@@ -0,0 +1,36 @@
+user1 = {
+    "id": 1,
+    "name": "Pavan Patharde",
+    "rank": 1,
+    "credits": 150,
+    "history": [{"1/1/2018" : 90}, {"2/1/2018" : 88}, {"3/1/2018" : 87}, {"4/1/2018" : 85}, {"5/1/2018" : 83}, {"6/1/2018" : 82}, {"7/1/2018" : 80}, {"8/1/2018" : 81}, {"9/1/2018" : 80}, {"10/1/2018" : 75}],
+    "electricity": 20,
+    "food": "veg",
+    "mobility": "mid",
+    "distance": 100,
+    "trees_planted": 2
+}
+
+user2 = {
+    "id": 2,
+    "name": "Yugnash Aggarwal",
+    "rank": 2,
+    "credits": 120,
+    "history": [{"1/1/2018" : 80}, {"2/1/2018" : 82}, {"3/1/2018" : 87}, {"4/1/2018" : 85}, {"5/1/2018" : 83}, {"6/1/2018" : 80}, {"7/1/2018" : 80}, {"8/1/2018" : 81}, {"9/1/2018" : 80}, {"10/1/2018" : 82}],
+    "electricity": 30,
+    "food": "non-veg",
+    "mobility": "mid",
+    "distance": 10,
+    "trees_planted": 5
+}
+
+stats = {
+    "trees_planted": 100,
+    "carbon": {
+        "food": 20,
+        "electricity": 20,
+        "mobility": 30
+    },
+    "shares": 1000,
+    "impact": "100 Kg reduced."
+}
